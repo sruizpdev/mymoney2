3bdcec39f8d30616e4018dabddf1f7f2a55c178a
Sergio Ruiz <sergiotto@outlook.com>
2023-12-17 12:32:25 +0100

    feat: add radio button to new expense component

--- src/app/pages/new-income/new-income.component.ts
+++ src/app/pages/new-income/new-income.component.ts
@@ -1,12 +1,49 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-new-income',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './new-income.component.html',
   styleUrl: './new-income.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NewIncomeComponent {}
+export class NewIncomeComponent {
+  newIncomeForm = new FormGroup({
+    amount: new FormControl('', [Validators.required]),
+    day: new FormControl(''),
+    month: new FormControl(''),
+    year: new FormControl(''),
+    type: new FormControl('', [Validators.required]),
+    notes: new FormControl('', [Validators.required]),
+  });
+
+  onSubmit() {
+    const data = {
+      amount: this.newIncomeForm.value.amount,
+      day: this.newIncomeForm.value.day?.trim(),
+      month: this.newIncomeForm.value.month?.trim(),
+      year: this.newIncomeForm.value.year?.trim(),
+      type: this.newIncomeForm.value.type,
+      notes: this.newIncomeForm.value.notes?.trim(),
+    };
+    console.log(data);
+
+    // };
+    // this.userService
+    //   .addNew(data)
+    //   .then(() => {
+    //     this.router.navigate(['/home']);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+  }
+}
