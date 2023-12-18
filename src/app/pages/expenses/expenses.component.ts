d94ce49d4a98bb9515d4d74fd65d49d7bd6e4350
Sergio Ruiz <sergiotto@outlook.com>
2023-12-18 21:26:56 +0100

    feat: skeleton for expenses and incomes components

--- src/app/pages/expenses/expenses.component.ts
+++ src/app/pages/expenses/expenses.component.ts
@@ -19,4 +19,30 @@ export class ExpensesComponent {
       console.log(res);
     });
   }
+  daily = [
+    {
+      id: 1,
+      amount: 5.5,
+      notes: 'Biedronka',
+      type: 'paw.png',
+    },
+    {
+      id: 2,
+      amount: 25.15,
+      notes: 'Biedronka',
+      type: 'salary.png',
+    },
+    {
+      id: 3,
+      amount: 54.45,
+      notes: 'Kaufland',
+      type: 'bus-stop.png',
+    },
+    {
+      id: 4,
+      amount: 5.45,
+      notes: 'Kaufland',
+      type: 'sleeping-pills.png',
+    },
+  ];
 }
--- src/app/pages/incomes/incomes.component.ts
+++ src/app/pages/incomes/incomes.component.ts
@@ -4,11 +4,36 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'app-incomes',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule],
   templateUrl: './incomes.component.html',
   styleUrl: './incomes.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class IncomesComponent { }
+export class IncomesComponent {
+  daily = [
+    {
+      id: 1,
+      amount: 5.5,
+      notes: 'Biedronka',
+      type: 'paw.png',
+    },
+    {
+      id: 2,
+      amount: 25.15,
+      notes: 'Biedronka',
+      type: 'salary.png',
+    },
+    {
+      id: 3,
+      amount: 54.45,
+      notes: 'Kaufland',
+      type: 'bus-stop.png',
+    },
+    {
+      id: 4,
+      amount: 5.45,
+      notes: 'Kaufland',
+      type: 'sleeping-pills.png',
+    },
+  ];
+}
