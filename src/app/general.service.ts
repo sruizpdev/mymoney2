44d5cb8b6aff2c7571846fff4a4cd25f5d48613b
Sergio Ruiz <sergiotto@outlook.com>
2023-12-19 13:23:53 +0100

    feat: add copy funtion to service

--- src/app/general.service.ts
+++ src/app/general.service.ts
@@ -3,6 +3,8 @@ import { Injectable, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
 import {
+  DocumentData,
+  DocumentReference,
   Firestore,
   addDoc,
   collection,
@@ -18,7 +20,25 @@ export class GeneralService {
   constructor(private fs: Firestore) {}
 
   getAllExpenses() {
-    const collectionInstance = collection(this.fs, 'expenses');
+    const collectionInstance = collection(this.fs, 'origen');
     return collectionData(collectionInstance, { idField: 'id' });
+  }
+
+  copy() {
+    this.getAllExpenses().subscribe((res) => {
+      res.forEach((element) => {
+        const dbInstance = collection(this.fs, 'destino3');
+
+        const mergedDate = `${element['day']}-${element['month']}-${element['year']}`;
+
+        const elementWithoutId = { ...element };
+        delete elementWithoutId.id;
+        delete elementWithoutId['day'];
+        delete elementWithoutId['month'];
+        delete elementWithoutId['year'];
+
+        addDoc(dbInstance, { ...elementWithoutId, date: mergedDate });
+      });
+    });
   }
 }
--- src/app/pages/options/options.component.ts
+++ src/app/pages/options/options.component.ts
@@ -1,14 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { GeneralService } from '../../general.service';
 
 @Component({
   selector: 'app-options',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule],
   templateUrl: './options.component.html',
   styleUrl: './options.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class OptionsComponent { }
+export class OptionsComponent {
+  constructor(private generalService: GeneralService) {}
+
+  copiar(): void {
+    this.generalService.copy();
+  }
+}
