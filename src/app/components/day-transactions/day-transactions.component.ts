b85d6523299458aa51045ab99a2d27b14a1c2bf7
Sergio Ruiz <sergiotto@outlook.com>
2023-12-23 14:43:49 +0100

    feat: fix last movements component

--- src/app/components/day-transactions/day-transactions.component.ts
+++ src/app/components/day-transactions/day-transactions.component.ts
@@ -1,39 +1,67 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { Observable, of } from 'rxjs';
+import { GeneralService } from '../../general.service';
+import { RouterLink } from '@angular/router';
+interface DailyMovements {
+  day: string;
+  data: {
+    amount: number;
+    date: string;
+    id: string;
+    notes: string;
+    type: string;
+  };
+}
 @Component({
   selector: 'app-day-transactions',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterLink],
   templateUrl: './day-transactions.component.html',
   styleUrl: './day-transactions.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DayTransactionsComponent {
-  daily = [
-    {
-      id: 1,
-      amount: 5.5,
-      notes: 'Biedronka',
-      type: 'paw.png',
-    },
-    {
-      id: 2,
-      amount: 25.15,
-      notes: 'Biedronka',
-      type: 'salary.png',
-    },
-    {
-      id: 3,
-      amount: 54.45,
-      notes: 'Kaufland',
-      type: 'bus-stop.png',
-    },
-    {
-      id: 4,
-      amount: 5.45,
-      notes: 'Kaufland',
-      type: 'sleeping-pills.png',
-    },
-  ];
+  data$: Observable<any[]>;
+
+  constructor(private generalService: GeneralService) {
+
+
+    function getDate() {
+      const currentDate = new Date();
+      const year = currentDate.getFullYear();
+      const month = String(currentDate.getMonth() + 1).padStart(2, '0'); 
+      const day = String(currentDate.getDate()).padStart(2, '0');
+      
+      const lastDay = new Date(year, Number(month), 0).getDate();
+      return {year,month,day,lastDay};
+    }
+    
+    
+    const today = getDate();
+   
+
+    this.data$ = generalService.getExpenses(`${today.year}-${today.month}-${today.day}`, `${today.year}-${today.month}-${today.day}`);
+    this.data$.subscribe((res) => {
+      console.log(res);
+
+      const newArrayMovements: DailyMovements[] = res.reduce((result, item) => {
+        const existingItem = result.find(
+          (elem: { day: string }) => elem.day === item.date
+        );
+        if (existingItem) {
+          existingItem.data.push(item);
+        } else {
+          result.push({
+            day: item.date,
+            data: [item],
+          });
+        }
+        return result;
+      }, []);
+
+      console.log(newArrayMovements);
+      this.data$ = of(newArrayMovements);
+    });
+  }
 }
