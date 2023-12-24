410c8d6812913f365f0715815aff130405024d1b
Sergio Ruiz <sergiotto@outlook.com>
2023-12-24 17:22:09 +0100

    feat: add currentDay to expenses resume

--- src/app/components/total-expense/total-expense.component.ts
+++ src/app/components/total-expense/total-expense.component.ts
@@ -1,14 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { GeneralService } from '../../general.service';
 
 @Component({
   selector: 'app-total-expense',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule],
   templateUrl: './total-expense.component.html',
   styleUrl: './total-expense.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TotalExpenseComponent { }
+export class TotalExpenseComponent {
+  currentDay: string = '';
+  constructor(private generalService: GeneralService) {
+    const date = generalService.getDate();
+    this.currentDay = `${date.day}-${date.month}-${date.year}`
+  }
+}
