bacd58fa4c74a51a0b676d4e5fe47c2be2028b28
Sergio Ruiz <sergiotto@outlook.com>
2023-12-25 13:09:39 +0100

    feat: add resume calc

--- src/app/components/resume/resume.component.ts
+++ src/app/components/resume/resume.component.ts
@@ -1,14 +1,42 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { GeneralService } from '../../general.service';
+import { Observable, combineLatest, map, of } from 'rxjs';
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ResumeComponent { }
+export class ResumeComponent {
+  currentDay: string = '';
+  firstDayOfMonth: string = '';
+  dayFormated: string = '';
+
+  total$!: Observable<number>;
+  totalExpenses$!: Observable<number>;
+  totalIncomes$!: Observable<number>;
+
+  constructor(private generalService: GeneralService) {
+    const date = generalService.getDate();
+    this.currentDay = `${date.year}-${date.month}-${date.day}`;
+    this.firstDayOfMonth = `${date.year}-${date.month}-01`;
+    this.dayFormated = `${date.day}-${date.month}-${date.year}`;
+
+    this.totalExpenses$ = generalService.getTotalExpenses(
+      this.firstDayOfMonth,
+      this.currentDay
+    );
+    this.totalIncomes$ = generalService.getTotalIncomes(
+      this.firstDayOfMonth,
+      this.currentDay
+    );
+
+    this.total$ = combineLatest([this.totalIncomes$, this.totalExpenses$]).pipe(
+      map(([totalIncomes, totalExpenses]) => totalIncomes - totalExpenses)
+    );
+  }
+}
